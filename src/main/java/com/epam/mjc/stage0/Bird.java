0e39e68c200681e3b6c74debfee73f6df44d614c
Akzhol <cheder911@gmail.com>
2023-08-07 19:37:30 +0600

    1

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,18 +1,12 @@
 package com.epam.mjc.stage0;
 
 public class Bird extends Animal {
-
-    private String color;
-    private int numberOfPaws;
-    private boolean hasFur;
-    public Bird(String color, int numberOfPaws, boolean hasFur) {
-        super(color="blue", numberOfPaws=2, hasFur=false);
+    public Bird() {
+        super("blue", 2, false);
     }
-
     @Override
     public String getDescription() {
-        return   ("This animal is mostly " + this.color+". " + "It has " + this.numberOfPaws + (this.numberOfPaws==1?" paw":" paws") +  " and " +
-                (hasFur?"a":"no") + " fur. Moreover, it has 2 wings and can fly.");
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
     }
-    Bird bird=new Bird("blue",2,false);
+
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,15 +1,18 @@
 package com.epam.mjc.stage0;
 
+
 public class Dog extends Animal {
 
-    public Dog(String color, int numberOfPaws, boolean hasFur) {
-        super(color="brown", numberOfPaws=4, hasFur=true);
+
+    public Dog() {
+        super("brown", 4, true);
+
+
     }
 
-    @Override
     public String getDescription() {
         return super.getDescription();
     }
-    Dog dog=new Dog("brown",4,true);
+
 }
 
